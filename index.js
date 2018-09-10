9ee815aa1b74bedd6c80332ce2c848cf04266e1a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-10 17:47:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+// Define a function getFirstSelector(selector), which accepts a selector and returns the first element that matches.
